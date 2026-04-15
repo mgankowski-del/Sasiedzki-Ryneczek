a928293f73fab2afafbe9c72a1f380f2b44efc32
Michał Gankowski <68109464+cki-gh-michal-gankowski@users.noreply.github.com>
2026-04-15 16:23:04 +0200

    Wdrożenie PWA, powiadomień Push i automatycznego wygasania ofert

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD_cuGXokb55W6W4aB-QkV0c_jAqXkJQgk",
+    authDomain: "sasiedzki-ryneczek.firebaseapp.com",
+    projectId: "sasiedzki-ryneczek",
+    storageBucket: "sasiedzki-ryneczek.firebasestorage.app",
+    messagingSenderId: "885991041208",
+    appId: "1:885991041208:web:3df60bebb747b563f86c4d"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://via.placeholder.com/192'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
